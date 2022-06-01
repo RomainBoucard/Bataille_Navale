6604c11f0dc392cab0514f1b765a2e06d0db3359
RomainBoucard <101243946+RomainBoucard@users.noreply.github.com>
2022-06-01 21:00:29 +0000

    Classe Joueur

--- .ccls-cache/@home@runner@BatailleNavale/main.cpp
+++ .ccls-cache/@home@runner@BatailleNavale/main.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
 #include "Bateau.h"
+#include "Joueur.h"
 
 int main() {
   std::cout << "Hello World!\n";
 
   Bateau *Bateau1 = new Bateau("Bateau 1", 5, "A1", 1);
+  Joueur *Joueur1 = new Joueur;
+  Joueur *Joueur2 = new Joueur;
 
   cout << "Nom : " << Bateau1->get_name() << endl; //Nom Bateau
   cout << "Taille : " << Bateau1->get_taille() << endl; //Taille Bateau
@@ -14,5 +17,10 @@ int main() {
   cout << "Colonnes : " << endl; //Colonnes cases Bateau
   Bateau1->get_Ys() ;
   cout << endl;
+
+  Joueur1->setName("Romain");
+  cout << "Nom : " << Joueur1->getName() << endl; //Nom Joueur
+  Joueur1->creerBateau(Joueur2);
+  Joueur1->affichageBateau(Joueur2);
 
 }
--- Makefile
+++ Makefile
@@ -1,6 +1,8 @@
-main: main.o Bateau.o
-	g++ -o main main.o Bateau.o
+main: main.o Bateau.o Joueur.o
+	g++ -o main main.o Bateau.o Joueur.o
 main.o: main.cpp
 	g++ -c main.cpp
 Bateau.o: Bateau.cpp
 	g++ -c Bateau.cpp
+Joueur.o: Joueur.cpp
+	g++ -c Joueur.cpp
